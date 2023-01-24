90917a9b796b3ca652b80c25a2b002b2e3302cee
Amber <do10@sheridancollege.ca>
2023-01-24 10:25:09 -0500

    address

--- src/student1/Student1.java
+++ src/student1/Student1.java
@@ -6,11 +6,21 @@ package student1;
 public class Student1 {
     private  String studentId;
     private String studentName; 
+    private String address; 
 
     public Student1(String studentId, String studentName) {
         this.studentId = studentId;
         this.studentName = studentName;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
 
     public String getStudentId() {
         return studentId;
